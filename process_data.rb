d5dbc5d614c0fc0c12fe7c83fe6ffac2cf24b544
Ryan B. Harvey <ryan.b.harvey@ted.com>
2017-05-20 16:54:14 -0700

    Added start of script to process data, and created variable to store data file location.

---
+++ process_data.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+# This sets a variable named `datafile` to have the value `data/data.csv`.
+# Variables allow us to save values once for later use. You can think of
+# a variable as a named container. When you assign a value to the variable,
+# you're putting that value in the container. When you use the variable later,
+# you're taking the value out temporarily. Values can be anything: numbers,
+# character strings, file objects, lists, and more.
+datafile = "data/data.csv"
